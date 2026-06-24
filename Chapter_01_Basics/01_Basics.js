b2133899b425b879e8ff008d843a15724a1f2040
pooja0452 <poojakumari.0452@gmail.com>
2026-06-25 00:16:26 +0530

    my 1st commit

---
+++ Chapter_01_Basics/01_Basics.js
@@ -0,0 +1,10 @@
+console.log("Hello, World!");
+
+let x=10;//let means creating a new variable
+console.log(x);
+
+console.log(process.platform);//process.platform is a built-in variable that returns the platform of the operating system
+
+console.log(process.arch);//process.arch is a built-in variable that returns the architecture of the operating system
+
+console.log("Node version :"+process.version);//process.version is a built-in variable that returns the version of Node.js
